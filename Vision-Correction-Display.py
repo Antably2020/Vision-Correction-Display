6123f8521a1d32befb3d72b8bf2b730cfcaec0ea
mahmoud zoair <73954049+mahmoudzoair@users.noreply.github.com>
2022-01-03 23:21:06 +0200

    camera inputs class and variables calculations

--- Vision-Correction-Display.py
+++ Vision-Correction-Display.py
@@ -22,4 +22,24 @@ class Display:
         S = np.linspace(-self.size()/2, self.size()/2, self.resolution()+1)
         for i in range(len(S)-1):
             x[i] = (S[i] + S[i+1])/2
-        return x;            
+        return x;           
+
+class Camera:
+    Do=250 #distance between camera and screen
+    def _init_(self,f,fStop,focus,resolution):
+        self.f=f # focal length
+        self.fStop=fStop
+        self.focus=focus
+        self.resolution=resolution
+    def aperture(self):
+        return(self.f/self.fStop)
+    def di(self):  # distance between camera sensor and lens 
+        return(self.f*self.focus/(self.focus-self.f))
+    def sensor_width(self,Do,screen_size):
+        return (screen_size/Do*self.di())    
+    def sampling(self):
+        x = np.zeros((self.resolution,1))
+        S = np.linspace(self.sensor_width(self.Do,display.size())/2, -self.sensor_width(self.Do,display.size())/2, self.resolution+1)
+        for i in range(len(S)-1):
+            x[i] = (S[i] + S[i+1])/2
+        return x; 

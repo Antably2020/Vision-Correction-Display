a449e4a7e0c163230a74d5aa3641034b7e9930bc
mahmoud zoair <73954049+mahmoudzoair@users.noreply.github.com>
2022-03-18 16:16:30 +0200

    Update reset.php

--- quiz/reset.php
+++ quiz/reset.php
@@ -2,12 +2,12 @@
 <?php
 session_start();
 session_destroy();
-header("http://localhost/Vision-Correction-Display/quiz/color-test.php");
+header("http://localhost/Vision-Correction-Display/quiz/index.php");
 
 ?>
 <script>
 window.onload = function() {
     // similar behavior as an HTTP redirect
-    window.location.replace("http://localhost/Vision-Correction-Display/quiz/color-test.php");
+    window.location.replace("http://localhost/Vision-Correction-Display/quiz/index.php");
 }
 </script>

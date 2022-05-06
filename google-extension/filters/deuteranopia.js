f76bf944d7a97d0d54a1b448d6097b2297c4d639
mahmoud zoair <mahmoudzoair@gmail.com>
2022-05-06 04:43:45 +0200

    correction

--- google-extension/filters/deuteranopia.js
+++ google-extension/filters/deuteranopia.js
@@ -13,9 +13,28 @@ filterID.id = "filterID471924";
 filterID.setAttribute('style', 'height: 0; padding: 0; margin: 0; line-height: 0;');
 document.body.appendChild(filterID);
 
-filterID.innerHTML = '<svg id="colorblind-filters" style="display: none"> <defs> <filter id="deuteranopia" color-interpolation-filters="linearRGB"> <feColorMatrix type="matrix" values="0.625,0.375,0,0,0, 0.7,0.3,0,0,0, 0,0.3,0.7,0,0, 0,0,0,1,0" in="SourceGraphic" /> </filter> </defs> </svg>';
+filterID.innerHTML = '<svg id="colorblind-filters" style="display: none"> <defs> <filter id="deuteranopia" color-interpolation-filters="linearRGB"> <feColorMatrix type="matrix" values="0.000  0.041  0.555  0.000  0.000    0.000  1.000  0.000  0.000  0.000   0.256  0.170  0.000  0.000  0.000   0.000  0.000  0.000  1.000  0.000" in="SourceGraphic" /> </filter> </defs> </svg>';
 stylingID.innerHTML = 'html{-webkit-filter:url(#deuteranopia);-moz-filter:(#deuteranopia);-ms-filter:(#deuteranopia);-o-filter:(#deuteranopia);filter:(#deuteranopia);}'
 setTimeout(function() {
     window.scrollBy(1, 1);
     window.scrollBy(-1, -1);
 }, 1);
+
+/*
+
+
+
+<svg xmlns="http://www.w3.org/2000/svg">
+  <filter id="filter">
+    <feColorMatrix
+      type="matrix"
+      values=" 0.000  0.041  0.555  0.000  0.000 
+               0.000  1.000  0.000  0.000  0.000 
+               0.256  0.170  0.000  0.000  0.000 
+               0.000  0.000  0.000  1.000  0.000">
+    </feColorMatrix>
+  </filter>
+</svg>
+
+
+*/
--- google-extension/filters/protanopia.js
+++ google-extension/filters/protanopia.js
@@ -13,7 +13,7 @@ filterID.id = "filterID471924";
 filterID.setAttribute('style', 'height: 0; padding: 0; margin: 0; line-height: 0;');
 document.body.appendChild(filterID);
 
-filterID.innerHTML = '<svg id="colorblind-filters" style="display: none"> <defs> <filter id="protanopia" color-interpolation-filters="linearRGB"> <feColorMatrix type="matrix" values="0.000  0.000  1.000  0.000  0.000  0.500  0.500  0.000  0.000  0.000   1.000  0.000  0.000  0.000  0.000   0.000  0.000  0.000  1.000  0.000" in="SourceGraphic" /> </filter> </defs> </svg>';
+filterID.innerHTML = '<svg id="colorblind-filters" style="display: none"> <defs> <filter id="protanopia" color-interpolation-filters="linearRGB"> <feColorMatrix type="matrix" values="0.041  0.000  0.556  0.000  0.000  0.172  0.264  0.000  0.000  0.000   1.000  0.000  0.000  0.000  0.000  0.000  0.000  0.000  1.000  0.000" in="SourceGraphic" /> </filter> </defs> </svg>';
 stylingID.innerHTML = 'html{-webkit-filter:url(#protanopia);-moz-filter:(#protanopia);-ms-filter:(#protanopia);-o-filter:(#protanopia);filter:(#protanopia);}'
 setTimeout(function() {
     window.scrollBy(1, 1);
@@ -22,26 +22,14 @@ setTimeout(function() {
 
 /*
 
-0.567,0.433,0,0,0 
-0.558,0.442,0,0,0  
-0,0.242,0.758,0,0
-0,0,0,1,        0
-
-1,0,0,1,0  0.4447863,0.1,1.23443,1,0  0,0,1,1,0  0,0,0,1,0
-
-
-1,0,0,0,0 
-0.5, 0.5,0,0,0 
-0.25,0,0.25,0,0
-0,0,0,1,0
-
-
-
 <svg xmlns="http://www.w3.org/2000/svg">
   <filter id="filter">
     <feColorMatrix
       type="matrix"
-      values=" 0.000  0.000  1.000  0.000  0.000  0.500  0.500  0.000  0.000  0.000   1.000  0.000  0.000  0.000  0.000   0.000  0.000  0.000  1.000  0.000">
+      values=" 0.041  0.000  0.556  0.000  0.000 
+               0.172  0.264  0.000  0.000  0.000 
+               1.000  0.000  0.000  0.000  0.000 
+               0.000  0.000  0.000  1.000  0.000">
     </feColorMatrix>
   </filter>
 </svg>*/
